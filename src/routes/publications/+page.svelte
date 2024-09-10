5ac9bbbc24115aa269bcca6c50cf54b5580939fd
Retronyme <ruben.gres@proton.me>
2024-09-10 22:38:14 +0000

    shortened name

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -9,7 +9,7 @@
             <h4>
                 <a href="">
                     Le modèle Seg2Sat
-                </a> ATLAS IGN - A l'aire de l'intelligence artificielle
+                </a> — ATLAS IGN 2024
             </h4>
             A double spread presenting my work on Seg2Sat in the yearly IGN's Atlas
         </li>
@@ -20,7 +20,7 @@
             <h4>
                 <a href="">
                     L'IA générative au service de nos métiers et nos savoirs 
-                </a> ATLAS IGN - A l'aire de l'intelligence artificielle
+                </a> — ATLAS IGN 2024
             </h4>
             In this interview I explore the potential of RAGs for text analysis for geographical data
         </li>
@@ -33,7 +33,7 @@
             <h4>
                 <a href="https://connect.ed-diamond.com/misc/misc-134/techniques-des-injections-de-prompt-un-nouvel-eldorado-de-menaces-dirigees-vers-l-ia">
                     Techniques des injections de prompt
-                </a> - MISC n°134
+                </a> — MISC n°134
             </h4>
             In this article I explore the history of the main approaches to prompt injection and I propose a comparative study on different models
         </li>
