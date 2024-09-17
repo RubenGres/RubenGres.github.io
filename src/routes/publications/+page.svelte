7df7a19e9bdce1b51f16c796c1877c51055197ad
Retronyme <ruben.gres@proton.me>
2024-09-17 15:35:17 +0200

    added links to the atlas

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -7,18 +7,18 @@
     <ul>
         <li>
             <h4>
-                <a href="">
+                <a href="https://www.calameo.com/read/0011885824d49acbe5730?page=22">
                     Le modèle Seg2Sat
                 </a> — ATLAS IGN 2024
             </h4>
-            A double spread presenting my work on Seg2Sat in the yearly IGN's Atlas
+            A double spread presenting my work on Seg2Sat in the yearly IGN Atlas
         </li>
     </ul>
 
         <ul>
         <li>
             <h4>
-                <a href="">
+                <a href="https://www.calameo.com/read/0011885824d49acbe5730?page=75">
                     L'IA générative au service de nos métiers et nos savoirs 
                 </a> — ATLAS IGN 2024
             </h4>
