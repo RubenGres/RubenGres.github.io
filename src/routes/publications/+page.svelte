d580c76727e70dbe6c25e863ef85167511b61071
Retronyme <ruben.gres@proton.me>
2025-05-17 11:18:47 +0000

    Added CVPR publication

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -3,6 +3,18 @@
 <div id="page-wrap">
     <h1>Publications</h1>
 
+    <h2>Academic Involvement</h2>
+    <ul>
+        <li>
+            <h4>
+                <a href="https://arxiv.org/abs/2503.15683">
+                    The Change You Want To Detect: Semantic Change Detection In Earth Observation With Hybrid Data Generation
+                </a> — CVPR 2025
+            </h4>
+            While not listed as a co-author, I collaborated on the early development of this research and am acknowledged in the paper for contributing the original idea and providing technical feedback.
+        </li>
+    </ul>
+
     <h2>Non-Academic Publications</h2>
     <ul>
         <li>
@@ -21,7 +33,7 @@
                 </a> — MISC n°134
             </h4>
             In this article I explore the history of the main approaches to prompt injection and I propose a comparative study on different models
-             (top 10 best articles of 2024)
+             (top 10 best articles of 2024).
         </li>
     </ul>
 
@@ -33,18 +45,16 @@
                     Le modèle Seg2Sat
                 </a> — ATLAS IGN 2024
             </h4>
-            A double spread presenting my work on Seg2Sat in the yearly IGN's Atlas
+            A double spread presenting my work on Seg2Sat in the yearly IGN's Atlas.
         </li>
-    </ul>
-
-    <ul>
+        
         <li>
             <h4>
                 <a href="https://www.calameo.com/read/0011885824d49acbe5730?page=75">
                     L'IA générative au service de nos métiers et nos savoirs 
                 </a> — ATLAS IGN 2024
             </h4>
-            In this interview I explore the potential of RAGs for text analysis for geographical data
+            In this interview I explore the potential of RAGs for text analysis for geographical data.
         </li>
     </ul>
 
