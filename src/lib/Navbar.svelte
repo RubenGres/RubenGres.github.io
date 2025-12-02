542c31c09a6efca30dd5075ff1c0273bece4f1fa
Retronyme <ruben.gres@proton.me>
2025-12-02 14:55:57 +0000

    added goatcounter

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -28,6 +28,8 @@
   });
 </script>
 
+<script data-goatcounter="https://rgres.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+
 <div id="navigation">
   <nav class="navbar navbar-expand-sm navbar-light container">
     <div class="navbar-brand">
